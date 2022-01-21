438cee1f4078c6ab18a42c2c7317d1ba71b5390b
Sakthi Praba V <72703725+Sakthi134@users.noreply.github.com>
2022-01-21 17:18:01 +0530

    Update Railway.java

--- Railway.java
+++ Railway.java
@@ -1,41 +1,28 @@
-package com.sakthipraba;
+package com.sakthi_praba_V;
 import java.util.*;
-
-public class OnlineRailwaySystem {
+public class Main {
     static Scanner sc = new Scanner(System.in);
-    static String Admin_Id = "Admin";
-    static String Admin_Password = "1234";
+    static String Admin_Id = "Sakthi";
+    static String Admin_Password = "19072001";
     static int Admin_Attempt = 3;
-
     static int User_Id = 1, Current_User = -1;
-
     static ArrayList<Booking_S> Booking = new ArrayList<>();
     static ArrayList<Waiting_S> Waiting = new ArrayList<>();
-
     static int station[][] = new int[5][10];
-
-    static String place[] = { "Palce - 1", "Place - 2", "Place - 3", "Place - 4", "Place - 5", "Place - 6", "Place - 7" };
-
+    static String place[] = { "Place -> 1", "Place -> 2", "Place -> 3", "Place -> 4", "Place -> 5", "Place -> 6", "Place -> 7" };
     static int seat[][] = new int[10][7];
-
     static int waitseat[][] = new int[3][7];
     static int seatid = 1;
     static int bookingid = 1001;
-
-    // ArrayList of object
-    // For User
     static ArrayList<Creat_User_Obj> User_List = new ArrayList<>();
-
-    // Clear Screen
     static void Clr_scr() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
-
     static void Admin_Login() {
         while (true) {
             if (Admin_Attempt >= 0) {
-                System.out.print("Welcome to Admin Login\n\nEnter the Admin Id : ");
+                System.out.print("\t Welcome to Admin Login\n\nEnter the Admin Id : ");
                 String id = sc.next();
                 sc.nextLine();
                 System.out.print("Enter the Admin Password : ");
@@ -43,59 +30,62 @@ public class OnlineRailwaySystem {
                 sc.nextLine();
                 if (id.equals(Admin_Id) && pass.equals(Admin_Password)) {
                     Admin_Dashboard();
-                } else {
+                }
+                else {
                     Clr_scr();
                     Admin_Attempt -= 1;
-                    System.out.println("Id and Password missmatch Please try again ! You have only " + Admin_Attempt
-                            + " Attempt's");
+                    System.out.println("Id and Password mismatch Please try again ! You have only " + Admin_Attempt + " Attempt's");
                 }
-            } else {
+            }
+            else {
                 Clr_scr();
-                System.out.println("Your Account Has Been Locked :-)");
+                System.out.println("Your Account Has Been Locked !!!)");
                 main(null);
             }
         }
     }
-
     static void Admin_Dashboard() {
         while (true) {
             System.out.println("Welcome to the Admin Dashboard ! ");
-            System.out.println("1 => Add Number of Station & Seat\n2 => View Seat Availability\n3 => Back");
+            System.out.println("1 -> Add Number of Station & Seat\n2 -> View Seat Availability\n3 -> Back");
             int ch = sc.nextInt();
             if (ch == 1) {
-            } else if (ch == 2) {
+            }
+            else if (ch == 2) {
                 View_Availability();
-            } else if (ch == 3) {
+            }
+            else if (ch == 3) {
                 main(null);
-            } else {
-                System.out.println("Enter the valid Chooise !");
+            }
+            else {
+                System.out.println("Enter the valid Choose !");
             }
         }
     }
-
     static void View_Availability() {
     }
-
     static void User_Panel() {
         while (true) {
-            System.out.println("Welcome to User Panel : \n1 => Login \n2 => Register \n3 => Back");
+            System.out.println("\t Welcome to User Panel : \n1 -> Login \n2 -> Register \n3 -> Back");
             int ch = sc.nextInt();
             if (ch == 1) {
                 User_Login();
-            } else if (ch == 2) {
+            }
+            else if (ch == 2) {
                 User_Register();
-            } else if (ch == 3) {
+            }
+            else if (ch == 3) {
                 main(null);
-            } else {
+            }
+            else {
                 System.out.println("Enter the valid Number !");
             }
         }
     }
-
     static void User_Login() {
         while (true) {
             int k = 1;
-            System.out.print("Welcome to User Login \n\nEnter your User Id : ");
+            System.out.print("\t Welcome to User Login \n\nEnter your User Id : ");
             String id = sc.next();
             sc.nextLine();
             System.out.print("Enter Your Password : ");
@@ -115,10 +105,9 @@ public class OnlineRailwaySystem {
             }
         }
     }
-
     static void User_Register() {
         while (true) {
-            System.out.print("Welcome to user Regestration \nEnter your name : ");
+            System.out.print("\t Welcome to user Regestration \nEnter your name : ");
             String name = sc.next();
             sc.nextLine();
             System.out.print("Enter your Password : ");
@@ -130,10 +119,9 @@ public class OnlineRailwaySystem {
             User_Login();
         }
     }
-
     static void User_Dashboard() {
         while (true) {
-            System.out.println("Welcome To user Dashboard !");
+            System.out.println("\t Welcome To user Dashboard !");
             System.out.println("1 => Book Ticket\n2 => Delete Ticket\n3 => History\n4 => Back");
             int ch = sc.nextInt();
             if (ch == 1) {
@@ -149,23 +137,23 @@ public class OnlineRailwaySystem {
             }
         }
     }
-
     static void Ticket_Booking() {
         ab: while (true) {
             int seat1 = -1;
-            System.out.println("\tWellcome " + User_List.get(Current_User).U_Name);
-            System.out.println(
-                    "1.Palce - 1\n2.Place - 2\n3.Place - 3\n4.Place - 4\n5.Place - 5\n6.Place - 6\n7.Place - 7\n8.Exit");
+            System.out.println("\tWelcome " + User_List.get(Current_User).U_Name);
+            System.out.println("1->Palace - 1\n2->Place - 2\n3->Place - 3\n4->Place - 4\n5->Place - 5\n6->Place - 6\n7->Place - 7\n8->Exit");
             int start;
             while (true) {
                 System.out.println("Enter Starting Point");
                 start = sc.nextInt();
                 if (start > 0 && start < 8) {
                     break;
-                } else if (start == 8) {
+                }
+                else if (start == 8) {
                     break ab;
-                } else {
-                    System.out.println("Invalid Starting Ponit...");
+                }
+                else {
+                    System.out.println("Invalid Starting Pointt...");
                 }
             }
             int landing;
@@ -174,10 +162,12 @@ public class OnlineRailwaySystem {
                 landing = sc.nextInt();
                 if (landing < 8 && landing > start) {
                     break;
-                } else if (landing == 8) {
+                }
+                else if (landing == 8) {
                     break ab;
-                } else {
-                    System.out.println("Invalid Landing Ponit...");
+                }
+                else {
+                    System.out.println("Invalid Landing Point...");
                 }
             }
             int count = 0;
@@ -200,15 +190,12 @@ public class OnlineRailwaySystem {
                             seat1 = j;
                             seat2 = 0;
                         }
-
                     }
                     if (count == 0) {
 
                         break a;
                     }
-
                 }
-
                 for (int j = 0; j < 3; j++) {
                     count = 0;
                     for (int t = 0; t < 7; t++) {
@@ -223,37 +210,31 @@ public class OnlineRailwaySystem {
                             seat1 = j;
                             seat2 = 1;
                         }
-
                     }
                     if (count == 0) {
-
                         break a;
                     }
-
                 }
             }
             seatid++;
             if (seat1 != -1 && seat2 == 0) {
-                Booking.add(new Booking_S(User_List.get(Current_User).U_Id, (seat1 + 1), bookingid, place[start - 1],
-                        place[landing - 1], start, landing, "Seat Alloted"));
-                System.out.println("Seat Alloted :" + (seat1 + 1));
+                Booking.add(new Booking_S(User_List.get(Current_User).U_Id, (seat1 + 1), bookingid, place[start - 1], place[landing - 1], start, landing, "Seat Alloted"));
+                System.out.println("Seat Allotted :" + (seat1 + 1));
                 System.out.println("Booking id :" + bookingid);
                 bookingid++;
-            } else if (seat1 != -1 && seat2 == 1) {
-                Waiting.add(new Waiting_S(User_List.get(Current_User).U_Id, (seat1 + 1), bookingid, place[start - 1],
-                        place[landing - 1], start, landing, "Seat Waitinglist"));
-                System.out.println("WaitSeat Alloted :" + (seat1 + 1));
+            }
+            else if (seat1 != -1 && seat2 == 1) {
+                Waiting.add(new Waiting_S(User_List.get(Current_User).U_Id, (seat1 + 1), bookingid, place[start - 1], place[landing - 1], start, landing, "Seat Waitinglist"));
+                System.out.println("WaitSeat Allotted :" + (seat1 + 1));
                 System.out.println("Booking id :" + bookingid);
-
                 bookingid++;
-            } else {
-                System.out.println("Seat Unavalable");
+            }
+            else {
+                System.out.println("Seat Unavailable");
             }
         }
     }
-
     static void Ticket_Canceling() {
-
         System.out.println("\tWelcome" + User_List.get(Current_User).U_Name+" !");
         System.out.println("Enter Booking ID:");
         int check = sc.nextInt();
@@ -272,7 +253,6 @@ public class OnlineRailwaySystem {
                     break b;
                 }
                 Booking.remove(j);
-                // allotedseat.remove(j);
                 int ws = 0, wl = 0;
                 a: for (int g = 0; g < Waiting.size(); g++) {
                     for (int k = 0; k < 10; k++) {
@@ -285,7 +265,6 @@ public class OnlineRailwaySystem {
                         }
                         for (int t = Waiting.get(g).Starting_Id - 1; t < check1; t++) {
                             if (count == 0) {
-
                                 seat[k][t] = seatid;
                                 ws = Waiting.get(g).Starting_Id - 1;
                                 wl = Waiting.get(g).Landing_Id - 1;
@@ -295,7 +274,6 @@ public class OnlineRailwaySystem {
 
                         }
                         if (count == 0) {
-
                             break a;
                         }
 
@@ -303,33 +281,29 @@ public class OnlineRailwaySystem {
                 }
                 if (seat1 != -1) {
                     Booking.add(new Booking_S(User_List.get(Current_User).U_Id, (seat1+1), Waiting.get(j).Booking_Id_List, Waiting.get(j).Starting_Point, Waiting.get(j).Landing_Point, Waiting.get(j).Starting_Id, Waiting.get(j).Landing_Id, "Seat Alloted"));
-                    System.out.println("Seat Alloted :" + (seat1 + 1));
+                    System.out.println("Seat Allotted :" + (seat1 + 1));
                     System.out.println("Booking id :" + Waiting.get(j).Booking_Id_List);
                     bb: for (int y = st; y < 3;) {
                         for (int t = 0; t < 7; t++) {
                             if (t >= ws && t < wl) {
                                 waitseat[y][t] = 0;
-
                             }
                         }
                         break bb;
                     }
                     Waiting.remove(st);
                 }
-
             }
         }
-
     }
-
     static void History() {
         int k = 0;
         for (int i = 0; i <Booking.size(); i++) {
             if (Booking.get(i).User_Id == User_List.get(Current_User).U_Id) {
                 System.out.println("Ticket Number : " + (++k));
                 System.out.println("Starting Point : " + Booking.get(i).Starting_Point);
-                System.out.println("Lnding Point : " + Booking.get(i).Landing_Point);
-                System.out.println("Steat Number : " + Booking.get(i).Alloted_Seat);
+                System.out.println("Landing Point : " + Booking.get(i).Landing_Point);
+                System.out.println("Seat Number : " + Booking.get(i).Alloted_Seat);
                 System.out.println("Booking Id : " + Booking.get(i).Booking_Id_List);
                 System.out.println("Status : " + Booking.get(i).Status);
                 System.out.println("--------------------------------------------");
@@ -340,50 +314,45 @@ public class OnlineRailwaySystem {
             if (Waiting.get(i).User_Id == User_List.get(Current_User).U_Id) {
                 System.out.println("Ticket Number : " + (++k));
                 System.out.println("Starting Point : " + Waiting.get(i).Starting_Point);
-                System.out.println("Lnding Point : " + Waiting.get(i).Landing_Point);
-                System.out.println("Steat Number : " + Waiting.get(i).Alloted_Seat);
+                System.out.println("Landing Point : " + Waiting.get(i).Landing_Point);
+                System.out.println("Seat Number : " + Waiting.get(i).Alloted_Seat);
                 System.out.println("Waiting Id : " + Waiting.get(i).Booking_Id_List);
                 System.out.println("Status : " + Waiting.get(i).Status);
                 System.out.println("--------------------------------------------");
             }
         }
     }
-
     public static void main(String[] args) {
-
         while (true) {
-            System.out.println("Welcome To Online Reservation System !");
-            System.out.println("1 => Admin\n2 => User\n3 => Exit");
+            System.out.println("\t Welcome To Online Reservation System !");
+            System.out.println("1 -> Admin\n2 -> User\n3 -> Exit");
             int Ch = sc.nextInt();
             if (Ch == 1) {
                 Admin_Login();
-            } else if (Ch == 2) {
+            }
+            else if (Ch == 2) {
                 User_Panel();
-            } else if (Ch == 3) {
+            }
+            else if (Ch == 3) {
                 System.exit(0);
-            } else {
+            }
+            else {
                 System.out.println("Enter the valid Number !");
             }
         }
-
     }
 }
-
 class Creat_User_Obj {
-
     String U_Name, U_Password;
     int U_Id;
     List<String> List = new ArrayList<>();
-
     Creat_User_Obj(String name, String password, int id) {
         this.U_Name = name;
         this.U_Password = password;
         this.U_Id = id;
     }
 }
-
 class Booking_S {
-
     public int User_Id;
     public int Alloted_Seat;
     public int Booking_Id_List;
@@ -392,9 +361,7 @@ class Booking_S {
     public int Starting_Id;
     public int Landing_Id;
     public String Status;
-
-    public Booking_S(int User_Id, int Alloted_Seat, int Booked_Id, String StartPt, String EndPt, int St_Id, int Ld_Id,
-            String Status) {
+    public Booking_S(int User_Id, int Alloted_Seat, int Booked_Id, String StartPt, String EndPt, int St_Id, int Ld_Id, String Status) {
         this.User_Id = User_Id;
         this.Alloted_Seat = Alloted_Seat;
         this.Booking_Id_List = Booked_Id;
@@ -405,7 +372,6 @@ class Booking_S {
         this.Status = Status;
     }
 }
-
 class Waiting_S {
     public int User_Id;
     public int Alloted_Seat;
@@ -415,9 +381,7 @@ class Waiting_S {
     public int Starting_Id;
     public int Landing_Id;
     public String Status;
-
-    public Waiting_S(int User_Id, int Alloted_Seat, int Booked_Id, String StartPt, String EndPt, int St_Id, int Ld_Id,
-            String Status) {
+    public Waiting_S(int User_Id, int Alloted_Seat, int Booked_Id, String StartPt, String EndPt, int St_Id, int Ld_Id, String Status) {
         this.User_Id = User_Id;
         this.Alloted_Seat = Alloted_Seat;
         this.Booking_Id_List = Booked_Id;
